4979010584a08039962b3e4904390e3a7f527994
kastastin <kostya242421@gmail.com>
2023-01-04 16:18:08 +0200

    required DOM elements + NodeList

--- 05-Modal/script.js
+++ 05-Modal/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+console.log(btnsOpenModal);
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  console.log(btnsOpenModal[i].textContent);
+}

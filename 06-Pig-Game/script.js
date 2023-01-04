49566deb33f409e60e04f47bda7b8cb29d752688
kastastin <kostya242421@gmail.com>
2023-01-04 20:54:57 +0200

    Switching the Active Player

--- 06-Pig-Game/script.js
+++ 06-Pig-Game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // <-- Selecting Elements -->
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.querySelector('#current--0');
@@ -12,16 +14,18 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// <-- Starting Conditions --> 
+// <-- Starting Conditions -->
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
 let rollCount = 0;
+let activePlayer = 0;
 
-// <-- Rolling dice functionality --> 
-btnRoll.addEventListener('click', function() {
+// <-- Rolling dice functionality -->
+btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
 
@@ -33,10 +37,20 @@ btnRoll.addEventListener('click', function() {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
     rollCount++;
-    currRoll0El.textContent = rollCount;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
+    document.querySelector(`#roll-count--${activePlayer}`).textContent =
+      rollCount;
   } else {
-    
+    // Switch to next player
+    currentScore = 0;
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    rollCount = 0;
+    document.querySelector(`#roll-count--${activePlayer}`).textContent = 0;
+
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });

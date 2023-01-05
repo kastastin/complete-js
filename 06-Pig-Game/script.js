95881e83fba559a7ec84bc840a901c866cdbc519
kastastin <kostya242421@gmail.com>
2023-01-06 01:03:49 +0200

    Holding Current Score

--- 06-Pig-Game/script.js
+++ 06-Pig-Game/script.js
@@ -14,20 +14,24 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const scores = [0, 0];
+const turns = [0, 0];
+let currentScore = 0;
+let rollCount = 0;
+let activePlayer = 0;
+
 // <-- Starting Conditions -->
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let rollCount = 0;
-let activePlayer = 0;
-
 // <-- Rolling dice functionality -->
-btnRoll.addEventListener('click', function () {
+function btnRollHandler() {
   // 1. Generating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
+  turns[activePlayer]++;
+  document.querySelector(`#turn--${activePlayer}`).textContent =
+    turns[activePlayer];
 
   // 2. Display dice
   diceEl.classList.remove('hidden');
@@ -43,14 +47,41 @@ btnRoll.addEventListener('click', function () {
     document.querySelector(`#roll-count--${activePlayer}`).textContent =
       rollCount;
   } else {
-    // Switch to next player
-    currentScore = 0;
-    document.querySelector(`#current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    rollCount = 0;
-    document.querySelector(`#roll-count--${activePlayer}`).textContent = 0;
-
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
-});
+}
+
+function btnHoldHandler() {
+  // 1. Add current score to active player's score
+  scores[activePlayer] += currentScore;
+  document.querySelector(`#score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  if (scores[activePlayer] >= 100) {
+    // Finish the game
+    diceEl.classList.add('hidden');
+    btnRoll.removeEventListener('click', btnRollHandler);
+    btnHold.removeEventListener('click', btnHoldHandler);
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  } else {
+    switchPlayer();
+  }
+}
+
+function switchPlayer() {
+  currentScore = 0;
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  rollCount = 0;
+  document.querySelector(`#roll-count--${activePlayer}`).textContent = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+}
+
+btnRoll.addEventListener('click', btnRollHandler);
+btnHold.addEventListener('click', btnHoldHandler);

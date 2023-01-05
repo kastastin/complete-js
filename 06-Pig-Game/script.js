94fa1790e393134283000903f17b981c40ddf232
kastastin <kostya242421@gmail.com>
2023-01-06 01:18:35 +0200

    Resetting game + refactoring

--- 06-Pig-Game/script.js
+++ 06-Pig-Game/script.js
@@ -1,29 +1,16 @@
 'use strict';
 
 // <-- Selecting Elements -->
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
-const score0El = document.querySelector('#score--0');
-const score1El = document.querySelector('#score--1');
-const current0El = document.querySelector('#current--0');
-const current1El = document.querySelector('#current--1');
-const currRoll0El = document.querySelector('#roll-count--0');
-const currRoll1El = document.querySelector('#roll-count--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-const scores = [0, 0];
 const turns = [0, 0];
-let currentScore = 0;
+const scores = [0, 0];
 let rollCount = 0;
+let currentScore = 0;
 let activePlayer = 0;
-
-// <-- Starting Conditions -->
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
 // <-- Rolling dice functionality -->
 function btnRollHandler() {
@@ -37,11 +24,9 @@ function btnRollHandler() {
   diceEl.classList.remove('hidden');
   diceEl.src = `img/dice-${dice}.png`;
 
-  // 3. Check for rolled 1
   if (dice !== 1) {
-    // Add dice to current score
-    currentScore += dice;
     rollCount++;
+    currentScore += dice;
     document.querySelector(`#current--${activePlayer}`).textContent =
       currentScore;
     document.querySelector(`#roll-count--${activePlayer}`).textContent =
@@ -74,14 +59,15 @@ function btnHoldHandler() {
 }
 
 function switchPlayer() {
+  rollCount = 0;
   currentScore = 0;
   document.querySelector(`#current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  rollCount = 0;
   document.querySelector(`#roll-count--${activePlayer}`).textContent = 0;
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+  document.querySelector('.player--0').classList.toggle('player--active');
+  document.querySelector('.player--1').classList.toggle('player--active');
 }
 
+btnNew.addEventListener('click', () => location.reload());
 btnRoll.addEventListener('click', btnRollHandler);
 btnHold.addEventListener('click', btnHoldHandler);

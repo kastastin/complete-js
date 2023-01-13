3135b26e78809a6b5f003bbc6c6de8c5b237cf4f
kastastin <kostya242421@gmail.com>
2023-01-13 12:07:07 +0200

    Bind Method

--- 09-Functions/functionMethods.js
+++ 09-Functions/functionMethods.js
@@ -47,3 +47,46 @@ book.apply(swiss, flightData);
 book.call(swiss, ...flightData);
 
 console.log(lufthansa, eurowings, swiss);
+
+// <-- Bind Method -->
+console.clear();
+
+// Method bind does not immediately call the function
+// Bind returns a new function where 'this' keyword is bound
+
+const bookEW = book.bind(eurowings); // bookEW = function with 'this' = eurowings
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(149, 'Steven Lewis');
+
+const bookEW23 = book.bind(eurowings, 23); // set in stone 'this' and first argument
+bookEW23('Alexa Porter');
+bookEW23('Oscar Watts');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  this.planes++;
+  console.log(`Planes: ${this.planes}`);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200)); // 20
+
+const addVAT = addTax.bind(null, 0.23); // always 23%, 'this' is not need -> null
+console.log(addVAT(100)); // 123
+
+// Challenge
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));

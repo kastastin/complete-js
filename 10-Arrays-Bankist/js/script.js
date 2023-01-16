138ae135cd9b9a7757d78b27c65b7d10df921562
kastastin <kostya242421@gmail.com>
2023-01-16 19:00:55 +0200

    Creating DOM Elements

--- 10-Arrays-Bankist/js/script.js
+++ 10-Arrays-Bankist/js/script.js
@@ -56,3 +56,21 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (movement, index) {
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">2 deposit</div>
+        <div class="movements__date">${index + 1} ${type}</div>
+        <div class="movements__value">${movement}</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);

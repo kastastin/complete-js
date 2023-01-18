cd68c19e9b34c0bc67cc1565c525bcfc0558e67d
kastastin <kostya242421@gmail.com>
2023-01-18 16:38:44 +0200

    Coding Challenge #3

--- 10-Arrays-Bankist/js/challenge.js
+++ 10-Arrays-Bankist/js/challenge.js
@@ -27,3 +27,17 @@ const calcAverageHumanAge = function (dogAges) {
 };
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]); // 220
+
+// <-- Coding Chalenge #3 -->
+console.clear();
+
+const calcAverageHumanAgeChaining = function (dogAges) {
+  const averageAdultHumanAge = dogAges
+    .map((age) => (age <= 2 ? age * 2 : age * 4 + 16))
+    .filter((age) => age >= 18)
+    .reduce((acc, age, _, { length }) => acc + age / length, 0);
+
+  console.log(averageAdultHumanAge);
+  return averageAdultHumanAge;
+};
+calcAverageHumanAgeChaining([5, 2, 4, 1, 15, 8, 3]); // 44

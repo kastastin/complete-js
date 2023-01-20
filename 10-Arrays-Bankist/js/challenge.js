daa42b2c9f961fec2e338440b101722ae9bfd133
kastastin <kostya242421@gmail.com>
2023-01-20 13:42:41 +0200

    Coding Challenge #4

--- 10-Arrays-Bankist/js/challenge.js
+++ 10-Arrays-Bankist/js/challenge.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// <-- Coding Chalenge #1 -->
+// <-- Coding Challenge #1 -->
 
 const juliaAges = [3, 5, 2, 12, 7];
 const kateAges = [4, 1, 15, 8, 3];
@@ -14,7 +14,7 @@ const checkDogs = function (firstArr, secondArr) {
 };
 checkDogs(juliaAges, kateAges);
 
-// <-- Coding Chalenge #2 -->
+// <-- Coding Challenge #2 -->
 
 const calcAverageHumanAge = function (dogAges) {
   const humanAges = dogAges.map((age) => (age <= 2 ? age * 2 : age * 4 + 16));
@@ -28,7 +28,7 @@ const calcAverageHumanAge = function (dogAges) {
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]); // 220
 
-// <-- Coding Chalenge #3 -->
+// <-- Coding Challenge #3 -->
 console.clear();
 
 const calcAverageHumanAgeChaining = function (dogAges) {
@@ -41,3 +41,59 @@ const calcAverageHumanAgeChaining = function (dogAges) {
   return averageAdultHumanAge;
 };
 calcAverageHumanAgeChaining([5, 2, 4, 1, 15, 8, 3]); // 44
+
+// <-- Coding Challenge #4 -->
+console.clear();
+
+const dogs = [
+  { weight: 22, currentFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, currentFood: 200, owners: ['Matilda'] },
+  { weight: 13, currentFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, currentFood: 340, owners: ['Michael'] }
+];
+
+// Task 1
+dogs.forEach(
+  (dog) => (dog.recommendedFood = Math.round(dog.weight ** 0.75 * 28))
+);
+
+// Task 2
+const sarahDog = dogs.find((dog) => dog.owners.includes('Sarah'));
+console.log(
+  `Sarah's dog is eating too ${
+    sarahDog.currentFood > sarahDog.recommendedFood ? 'much' : 'little'
+  }`
+);
+
+// Task 3
+const ownersEatTooMuch = dogs
+  .filter((dog) => dog.currentFood > dog.recommendedFood)
+  .flatMap((dog) => dog.owners);
+
+const ownersEatTooLittle = dogs
+  .filter((dog) => dog.currentFood < dog.recommendedFood)
+  .flatMap((dog) => dog.owners);
+console.log(ownersEatTooMuch, ownersEatTooLittle);
+
+// Task 4
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat to much`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat to little`);
+
+// Task 5
+console.log(dogs.some((dog) => dog.recommendedFood === dog.currentFood));
+
+// Task 6
+const checkEatingOkay = (dog) =>
+  dog.currentFood > dog.recommendedFood * 0.9 &&
+  dog.currentFood < dog.recommendedFood * 1.1;
+
+console.log(dogs.some(checkEatingOkay)); // true
+
+// Task 7
+console.log(dogs.filter(checkEatingOkay));
+
+// Task 8
+const sortedDogs = dogs
+  .slice()
+  .sort((a, b) => a.recommendedFood - b.recommendedFood);
+console.log(sortedDogs);

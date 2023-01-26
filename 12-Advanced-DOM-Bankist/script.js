d6688588ca12f8f0394f57dd42cc920cbe82da14
kastastin <kostya242421@gmail.com>
2023-01-26 18:53:36 +0200

    Implementing a Sticky Navigation: The Scroll Event

--- 12-Advanced-DOM-Bankist/script.js
+++ 12-Advanced-DOM-Bankist/script.js
@@ -96,3 +96,12 @@ const handleHover = function (event) {
 // Passing 'argument' into handler
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// <-- Sticky navigation -->
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function (event) {
+  this.window.scrollY > initialCoords.top
+    ? nav.classList.add('sticky')
+    : nav.classList.remove('sticky');
+});

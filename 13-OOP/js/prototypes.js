87a1d1a95ffe14a0d895207acb29a199416eec50
kastastin <kostya242421@gmail.com>
2023-01-28 17:47:45 +0200

    Prototypal Inheritance on Built-In Objects

--- 13-OOP/js/prototypes.js
+++ 13-OOP/js/prototypes.js
@@ -26,3 +26,37 @@ console.log(gena); // {firstName: 'Gena', birthYear: 2000}
 console.log(gena.species); // Homo Sapiens
 console.log(gena.hasOwnProperty('firstName')); // true
 console.log(gena.hasOwnProperty('species')); // false
+
+// <-- Prototypal Inheritance on Built-In-Objects -->
+console.clear();
+
+// Object.prototype (top of prototype chain)
+console.log(gena.__proto__);
+console.log(gena.__proto__.__proto__);
+console.log(gena.__proto__.__proto__.__proto__);
+
+console.log(Person.prototype.constructor);
+console.dir(Person.prototype.constructor);
+
+const arr = [1, 2, 3, 3, 2, 2];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype); // true
+
+// Example
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique()); // [1, 2, 3]
+
+const h1 = document.querySelector('h1');
+console.log(h1.__proto__); // HTMLHeadingElement
+console.log(h1.__proto__.__proto__); // HTMLElement
+console.log(h1.__proto__.__proto__.__proto__); // Element
+console.log(h1.__proto__.__proto__.__proto__.__proto__); // Node
+console.log(h1.__proto__.__proto__.__proto__.__proto__.__proto__); // EventTarget
+console.log(h1.__proto__.__proto__.__proto__.__proto__.__proto__.__proto__); // Object
+
+const fn = (x) => x + 1;
+console.log(fn.__proto__); // f
+console.log(fn.__proto__.__proto__); // Object
+console.log(fn.__proto__.__proto__.__proto__); // null

8b1d2f8171aa27d26b58ab342ff035b38df6834c
kastastin <kostya242421@gmail.com>
2023-01-29 13:52:38 +0200

    Inheritance Between Classes: ES6 Classes

--- 13-OOP/js/classesES6.js
+++ 13-OOP/js/classesES6.js
@@ -52,3 +52,41 @@ console.log(tom.age); // 20
 
 const bob = new Person('Bob', 1950); // alert: Bob is not a full name!
 console.log(bob); // fullName: undefined, birthYear: 1950
+
+// <-- Inheritance Between 'Classes': ES6 Classes -->
+console.clear();
+
+class Human {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2023 - this.birthYear);
+  }
+
+  static printTest() {
+    console.log('Test');
+  }
+}
+
+class Student extends Human {
+  constructor(firstName, birthYear, course) {
+    super(firstName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(2023 - this.birthYear + ' y.o [overridden method]');
+  }
+}
+
+const tim = new Student('Tim Connor', 2000, 'Front-end');
+tim.introduce();
+Student.printTest(); // Test
+tim.calcAge(); // 23 y.o [overridden method]

f4c386c6e1b5e95cf2e8a3a4dbc2ae9f30f55643
kastastin <kostya242421@gmail.com>
2023-01-29 12:45:30 +0200

    Inheritance Between Classes: Constructor Functions

--- 13-OOP/js/constructorFunctions.js
+++ 13-OOP/js/constructorFunctions.js
@@ -22,3 +22,39 @@ console.log(tom, bob);
 
 console.log(tom instanceof Person); // true
 console.log([1, 2] instanceof Array); // true
+
+// <-- Inheritance Between 'Classes': Constructor Functions -->
+console.clear();
+
+const Human = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Human.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Human.prototype is __proto__ of Student.prototype
+Student.prototype = Object.create(Human.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2000, 'Computer Science');
+mike.introduce();
+mike.calcAge(); // 23
+console.log(mike); // Human
+
+console.log(mike instanceof Student); // true
+console.log(mike instanceof Human); // true
+console.log(mike instanceof Object); // true
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor); // Student (was Human without ⬆️)

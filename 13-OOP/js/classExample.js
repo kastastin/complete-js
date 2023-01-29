fe5c1c5b95c4d5cee4015536e4850caf9871c3b8
kastastin <kostya242421@gmail.com>
2023-01-29 19:13:07 +0200

    Encapsulation: Private Class Fields and Methods (_)

--- 13-OOP/js/classExample.js
+++ 13-OOP/js/classExample.js
@@ -4,28 +4,32 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    this._pin = pin; // Protected Property
+    this._movements = []; // Protected Property
     this.locale = navigator.language;
 
     console.log(`${this.owner}, your account is opened!`);
   }
 
   // Public Interface
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(value) {
-    this.movements.push(value);
+    this._movements.push(value);
   }
 
   withdraw(value) {
     this.deposit(-value);
   }
 
-  approveLoan(value) {
+  _approveLoan(value) {
     return true;
   }
 
   requestLoan(value) {
-    if (this.approveLoan(value)) {
+    if (this._approveLoan(value)) {
       this.deposit(value);
       console.log('Loan approved');
     }
@@ -40,6 +44,11 @@ acc1.deposit(200);
 acc1.withdraw(100);
 
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+acc1._approveLoan(1000);
 
 console.log(acc1);
+
+// Encapsulation uses to prevent code from outside of a class to accidentally
+// manipulate data inside the class
+
+console.log(acc1.getMovements()); // [200, -100, 1000]

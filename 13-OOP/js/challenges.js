c88a6d92c5e3112672b29bf2bf232e0b31cb2c15
kastastin <kostya242421@gmail.com>
2023-01-29 13:33:49 +0200

    Coding Challenge #3

--- 13-OOP/js/challenges.js
+++ 13-OOP/js/challenges.js
@@ -65,3 +65,28 @@ console.log(ford.speedUS);
 
 ford.speedUS = 50;
 ford.displaySpeed();
+
+// <-- Coding Challenge #3 -->
+console.clear();
+
+const EV = function (make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+};
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function (chargeTo) {
+  this.charge = chargeTo;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge--;
+  console.log(`${this.make} speed: ${this.speed}, charge: ${this.charge}%`);
+};
+
+const tesla = new EV('Tesla', 120, 50);
+tesla.chargeBattery(80);
+tesla.brake(); // Tesla speed: 115 km/h
+tesla.accelerate(); // Tesla speed: 135, charge: 79 %

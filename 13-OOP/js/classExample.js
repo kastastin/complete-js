ebc7b459e4a710d899407c70eb10394162933e82
kastastin <kostya242421@gmail.com>
2023-01-29 19:44:06 +0200

    Encapsulation: Private Class Fields and Methods

--- 13-OOP/js/classExample.js
+++ 13-OOP/js/classExample.js
@@ -1,38 +1,51 @@
 'use strict';
 
+/* 
+    Encapsulation uses to prevent code from outside of a class
+    to accidentally manipulate data inside the class
+*/
+
 class Account {
+  // Public Fields (instances)
+  locale = navigator.language;
+
+  // Private Fields (instances)
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this._pin = pin; // Protected Property
-    this._movements = []; // Protected Property
-    this.locale = navigator.language;
+    this.#pin = pin; // Protected Property
 
     console.log(`${this.owner}, your account is opened!`);
   }
 
   // Public Interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(value) {
-    this._movements.push(value);
+    this.#movements.push(value);
   }
 
   withdraw(value) {
     this.deposit(-value);
   }
 
-  _approveLoan(value) {
-    return true;
-  }
-
-  requestLoan(value) {
-    if (this._approveLoan(value)) {
+  requestLoan(value, pin) {
+    if (this.#approveLoan(pin)) {
       this.deposit(value);
       console.log('Loan approved');
+    } else {
+      console.log('Error');
     }
+  }
+
+  // Private methods
+  #approveLoan(pin) {
+    return pin ? true : false;
   }
 }
 
@@ -43,12 +56,10 @@ const acc1 = new Account('Tom', 'EUR', 1111);
 acc1.deposit(200);
 acc1.withdraw(100);
 
-acc1.requestLoan(1000);
-acc1._approveLoan(1000);
+acc1.requestLoan(1000, 1111);
+// acc1.#approveLoan(1000); // SyntaxError
 
 console.log(acc1);
-
-// Encapsulation uses to prevent code from outside of a class to accidentally
-// manipulate data inside the class
-
 console.log(acc1.getMovements()); // [200, -100, 1000]
+// console.log(acc1.#movements); // SyntaxError
+// console.log(acc1.#pin); //SyntaxError: Private field '#pin' must be declared in an enclosing class

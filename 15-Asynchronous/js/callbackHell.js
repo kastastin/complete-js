519a4d35f62edfdbe4120a7bdd39bd6baba63161
kastastin <kostya242421@gmail.com>
2023-02-01 18:15:02 +0200

    Callback Hell

---
+++ 15-Asynchronous/js/callbackHell.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.svg}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.official}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}M people</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.values(data.languages)[0]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.values(data.currencies)[0].name
+      }</p>
+    </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndCountry = function (country) {
+  // AJAX Call Country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    renderCountry(data);
+    console.log(data);
+
+    // Get Neighbour Country
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX Call Country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndCountry('usa');
+
+// <-- Callback Hell -->
+setTimeout(function () {
+  console.log('1 second passed');
+  setTimeout(function () {
+    console.log('2 seconds passed');
+    setTimeout(function () {
+      console.log('3 seconds passed');
+      setTimeout(function () {
+        console.log('4 seconds passed');
+        setTimeout(function () {
+          console.log('5 seconds passed');
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);

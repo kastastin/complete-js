863192d410b3031dc775da29bfddd798d0563c8f
kastastin <kostya242421@gmail.com>
2023-02-05 22:48:08 +0200

    Introduction to NPM

--- 16-Modern-JS-Modules-Tooling/script.js
+++ 16-Modern-JS-Modules-Tooling/script.js
@@ -2,20 +2,20 @@
 // import add from './shoppingCart.js';
 // add('apple', 2); // 2 apple added to cart
 
-import {
-  addToCart,
-  totalPrice as price,
-  totalQuantity,
-  color
-} from './shoppingCart.js';
-import * as Shoppingcart from './shoppingCart.js';
+// import {
+//   addToCart,
+//   totalPrice as price,
+//   totalQuantity,
+//   color
+// } from './shoppingCart.js';
+// import * as Shoppingcart from './shoppingCart.js';
 
-console.log('Importing module');
+// console.log('Importing module');
 // console.log(shoppingCost); // ReferenceError: shoppingCost is not defined
 
-addToCart('bread', 5); // 5 bread added to cart
-console.log(price, totalQuantity, color); // 237 23 'red'
-console.log(Shoppingcart.color); // red
+// addToCart('bread', 5); // 5 bread added to cart
+// console.log(price, totalQuantity, color); // 237 23 'red'
+// console.log(Shoppingcart.color); // red
 
 // <-- Top-level await (ES2022) -->
 // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -39,33 +39,33 @@ const lastPost2 = await getLastPost();
 // console.log(lastPost2); // {...}
 
 // <-- Implementation of The Module Pattern -->
-const ShoppingCart2 = (function () {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
 
-  const addToCart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} added to cart`);
-  };
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
 
-  const orderStock = function (product, quantity) {
-    console.log(`${quantity} ${product} ordered from supplier`);
-  };
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier`);
+//   };
 
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity
-  };
-})();
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity
+//   };
+// })();
 
-ShoppingCart2.addToCart('toy', 4);
-ShoppingCart2.addToCart('pizza', 2);
-console.log(ShoppingCart2); // {...}
-console.log(ShoppingCart2.shippingCost); // undefined
+// ShoppingCart2.addToCart('toy', 4);
+// ShoppingCart2.addToCart('pizza', 2);
+// console.log(ShoppingCart2); // {...}
+// console.log(ShoppingCart2.shippingCost); // undefined
 
 // <-- CommonJS Modules -->
 // [Export] Not work in the browser, would work in Node.js
@@ -78,3 +78,19 @@ console.log(ShoppingCart2.shippingCost); // undefined
 
 // [Import] Not work in the browser, would work in Node.js
 // const { addToCart } = require('./shoppingCart.js');
+
+// <-- Introduction to NPM -->
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 3 },
+    { product: 'apple', quantity: 5 }
+  ],
+  user: { loggedIn: true }
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone, stateDeepClone);
